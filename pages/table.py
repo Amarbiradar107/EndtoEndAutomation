378cab44010ceb2090c8d29299c1bc98866dd2be
Amarbiradar107 <amarbiradar107@gmail.com>
2026-03-18 23:05:44 +0530

    combine test

--- pages/table.py
+++ pages/table.py
@@ -15,6 +15,7 @@ class TablePage:
     Min_enrollments_dropdown = "//div[@class='dropdown-button']"
     Min_enrollments_Options = "//div[@id='enrollDropdown']//li"
 
+
     log = Utilities().custom_logger()
 
     def __init__(self,setup_two):
@@ -36,15 +37,49 @@ class TablePage:
                             if data.text == "":
                                 self.log.info("blank value....")
                             else:
-                                assert data.text == "Java"
+                                assert data.text == user_filter
                         break
                     break
-        elif user_filter == "python":
-            self.driver.driver.find_element(By.XPATH,self.Language_filter_python).click()
-        else:
-            self.driver.driver.find_element(By.XPATH,self.Language_filter_any).click()
+
+        elif user_filter == "Python":
+            self.driver.find_element(By.XPATH,self.Language_filter_python).click()
+            column = len(self.driver.find_elements(By.XPATH, self.Tabel_header))
+            rows = len(self.driver.find_elements(By.XPATH, self.Tabel_body))
+            for col in range(1, column + 1):
+                column_header = self.driver.find_element(By.XPATH, "//table/thead/tr/th[" + str(col) + "]").text
+                if column_header == "Language":
+                    for row in range(1, rows + 1):
+                        col_data = self.driver.find_elements(By.XPATH, "//table/tbody/tr/td[" + str(col) + "]")
+                        len(col_data)
+                        # print(col_data.text)
+                        for data in col_data:
+                            if data.text == "":
+                                self.log.info("blank value....")
+                            else:
+                                assert data.text == user_filter
+                        break
+                    break
+        elif user_filter == "Any":
+            self.driver.find_element(By.XPATH,self.Language_filter_any).click()
+            column = len(self.driver.find_elements(By.XPATH, self.Tabel_header))
+            rows = len(self.driver.find_elements(By.XPATH, self.Tabel_body))
+            for col in range(1, column + 1):
+                column_header = self.driver.find_element(By.XPATH, "//table/thead/tr/th[" + str(col) + "]").text
+                if column_header == "Language":
+                    for row in range(1, rows + 1):
+                        col_data = self.driver.find_elements(By.XPATH, "//table/tbody/tr/td[" + str(col) + "]")
+                        len(col_data)
+                        # print(col_data.text)
+                        for data in col_data:
+                            if data.text == "":
+                                self.log.info("blank value....")
+                            else:
+                                assert data.text == user_filter
+                        break
+                    break
 
     def select_level_filter(self):
+        beginner_check_box = self.driver.find_element(By.XPATH,self.Level_filter_Beginner)
         intermediate_check_box = self.driver.find_element(By.XPATH,self.Level_filter_Intermediate).is_selected()
         advance_level_checkbox = self.driver.find_element(By.XPATH,self.Level_filter_Advanced).is_selected()
         if intermediate_check_box and advance_level_checkbox:
@@ -73,6 +108,7 @@ class TablePage:
         else:
             self.log.info("intermediate level selected")
             self.log.info("advanced level selected")
+
 
     def min_enrollments(self):
         click_dropdown = self.driver.find_element(By.XPATH,self.Min_enrollments_dropdown)
@@ -103,8 +139,43 @@ class TablePage:
                 break
 
 
-    def combined_filters(self):
-        pass
+    def combined_filters(self,user_filter):
+        # self.click_language_filter(user_filter)
+        # self.select_level_filter()
+        # self.min_enrollments()
+        if user_filter == "Python":
+            self.driver.find_element(By.XPATH, self.Language_filter_python).click()
+            intermediate_check_box = self.driver.find_element(By.XPATH, self.Level_filter_Intermediate).is_selected()
+            advance_level_checkbox = self.driver.find_element(By.XPATH, self.Level_filter_Advanced).is_selected()
+            if intermediate_check_box and advance_level_checkbox:
+                self.driver.find_element(By.XPATH, self.Level_filter_Intermediate).click()
+                self.log.info("intermediate level unselected")
+                self.driver.find_element(By.XPATH, self.Level_filter_Advanced).click()
+                self.log.info("advanced level unselected")
+                click_dropdown = self.driver.find_element(By.XPATH, self.Min_enrollments_dropdown)
+                click_dropdown.click()
+                dropdown_values = self.driver.find_elements(By.XPATH, self.Min_enrollments_Options)
+                self.log.info((len(dropdown_values)))
+                for value in dropdown_values:
+                    if value.text == "10,000+":
+                        value.click()
+                        self.log.info("10,000+ selected")
+                        column = len(self.driver.find_elements(By.XPATH,self.Tabel_header))
+                        rows = len(self.driver.find_elements(By.XPATH,self.Tabel_body))
+                        for col in range(1,column+1):
+                            column_header = self.driver.find_element(By.XPATH,"//table/thead/tr/th["+str(col)+"]").text
+                            col_data = self.driver.find_elements(By.XPATH,"//table/tbody/tr/td["+str(col)+"]")
+                            print(len(col_data))
+                            # print(col_data.text)
+                            for data in col_data:
+                                if data.text == "":
+                                    self.log.info("blank value....")
+                                else:
+                                    # assert data.text == user_filter
+                                    self.log.info(data.text)
+
+
+
 
     def no_results_state(self):
         pass
--- testcases/test_table_example.py
+++ testcases/test_table_example.py
@@ -19,7 +19,7 @@ class TestTableExample:
         tb.select_level_filter()
         time.sleep(2)
 
-    # @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
+    @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
     def test_min_enrollments(self,setup_two):
         tb = TablePage(setup_two)
         tb.min_enrollments()
@@ -27,20 +27,24 @@ class TestTableExample:
 
     def test_combine_filter(self,setup_two):
         tb = TablePage(setup_two)
-        tb.combined_filters()
+        tb.combined_filters("Python")
 
+    @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
     def test_no_results_state(self,setup_two):
         tb = TablePage(setup_two)
         tb.no_results_state()
 
+    @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
     def test_reset_button_visibility_and_behavior(self,setup_two):
         tb = TablePage(setup_two)
         tb.reset_button_visibility_and_behavior()
 
+    @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
     def test_sort_by_enrollments_ascending_numeric(self,setup_two):
         tb = TablePage(setup_two)
         tb.sort_by_enrollments_ascending_numeric()
 
+    @pytest.mark.skip(reason="This test is skipped because it's just an example and not meant to be executed.")
     def test_sort_by_course_name_alphabetical(self,setup_two):
         tb = TablePage(setup_two)
         tb.sort_by_course_name_alphabetical()
